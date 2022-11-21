fd925fa7ca51d48be64147e2db03cd664cc8377d
Federico  de Fortuny <federico.defortuny@cencosud.com>
2022-11-21 15:16:43 -0300

    primer commit en rama fede

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -7,12 +7,14 @@ export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
   async executeSeed(){
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1');
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
     data.results.forEach(({name, url}) => {
       const segments = url.split('/');
       const no: number = Number(segments[segments.length -2]);
-      console.log(no)
+      console.log(no);
     })
+
+    
     return data.results;
   }
 
