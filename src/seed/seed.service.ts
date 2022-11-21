4ff94f79e1e1da3e75fee1932a1e9a67802606d3
Federico  de Fortuny <federico.defortuny@cencosud.com>
2022-11-21 15:05:04 -0300

    antes de insertar por lotes

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed(){
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1');
+    data.results.forEach(({name, url}) => {
+      const segments = url.split('/');
+      const no: number = Number(segments[segments.length -2]);
+      console.log(no)
+    })
+    return data.results;
+  }
+
+}
